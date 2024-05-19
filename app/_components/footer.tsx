115aece9e4ea260086b2726bf82aa957a0cc2072
Fellipo <hfellipo@gmail.com>
2024-05-19 18:52:27 -0300

    feat: footer 2

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-        <div className="w-full bg-seondary py-6 px-5 text-center">
+        <div className="w-full bg-secondary py-6 px-5 text-center">
             <p className="text-gray-400 text-xs font-bold opacity-75">2024 AppLUDO o app dos Cabeludos.</p>
         </div>
     );
