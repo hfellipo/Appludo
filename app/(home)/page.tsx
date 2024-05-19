9b442c8b1743cdf61be1fa68d5dcde2e092a3481
Fellipo <hfellipo@gmail.com>
2024-05-19 00:15:42 -0300

    Search input

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,12 +7,11 @@ export default function Home() {
   return (
     <div>
       <Header />
-
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá Fellipo.</h2>
-      <p className="capitalize text-sm">{format(new Date(), "EEEE', ' dd 'de' MMMM", {
+        <p className="capitalize text-sm">{format(new Date(), "EEEE', ' dd 'de' MMMM", {
         locale: ptBR,
-      })}</p>
+        })}</p>
       </div>
     </div>
   );
